2f03401c592923ac0b025a8881c7d24ccbf6b7d5
mattomar <moddathero@gmail.com>
2024-09-11 20:32:51 +0700

    added the responsive design

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const TerserPlugin = require('terser-webpack-plugin');
+ 
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production', // Set mode to 'production'
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.bundle.js',
@@ -41,14 +43,22 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        // Additional options for TerserPlugin can go here
+      }),
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css", // Corrected to output the CSS file in the root of 'dist'
+      filename: '[name].css',
     }),
   ],
 };
+  
